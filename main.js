6f07dce2a5376ca789444e7ade34fef5e3e7c3ee
Manolache Bogdan-Gabriel <73283939+ManolacheBogdan@users.noreply.github.com>
2023-08-10 00:24:41 +0300

    Verificare finala ziua 2

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import {renderHomePage} from './src/components/renders';
-import {renderOrdersPage} from './src/components/renders';
-import {fetchEventData} from './src/components/fetchEventData';
+import {renderHomePage, renderOrdersPage} from './components/renders';
+import {fetchEventData} from './components/fetchEventData';
 
 
 // Navigate to a specific URL
@@ -26,6 +25,21 @@ function getOrdersPageTemplate() {
     </div>
     `;
 }
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('increment')) {
+    const quantityLabel = target.parentElement.querySelector('.quantity-label');
+    quantityLabel.textContent = parseInt(quantityLabel.textContent) + 1;
+  } else if (target.classList.contains('decrement')) {
+    const quantityLabel = target.parentElement.querySelector('.quantity-label');
+    const currentQuantity = parseInt(quantityLabel.textContent);
+    if (currentQuantity > 0) {
+      quantityLabel.textContent = currentQuantity - 1;
+    }
+  }
+});
+
 
 function setupNavigationEvents() {
   const navLinks = document.querySelectorAll('nav a');
@@ -67,6 +81,7 @@ async function renderContent(url) {
 
   if (url === '/') {
     const eventData = await fetchEventData();
+    console.log(eventData);
     renderHomePage(eventData);
   } else if (url === '/orders') {
     renderOrdersPage();
